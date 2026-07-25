4d82241b66903ed6ea6b1a9850afd378ef431d29
cjean <cjeancito121212@gmail.com>
2026-07-25 16:52:35 -0500

    MarcoLegal22

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ import Privacy from "./pages/Privacy";
 import Terms from "./pages/Terms";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import ScrollToTop from "./components/ScrollToTop";
+
 
 function App() {
 
   return (
 
     <BrowserRouter>
-      <ScrollToTop />
+
       <Routes>
 
         <Route
